6046e47afbc2f9532f813d6ddcfeafed60de264e
Aishwarya9813 <shingareaish13@gmail.com>
2025-08-17 19:04:03 +0530

    Selenium Basics

--- src/test/java/com/thetestingacademy/ex_01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/com/thetestingacademy/ex_01_Selenium_Basics/TestSelenium01.java
@@ -1,4 +1,24 @@
 package com.thetestingacademy.ex_01_Selenium_Basics;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestSelenium01 {
+
+    @Test
+    public void test_Code(){
+        Assert.assertEquals("Aishwarya","aishwarya");
+    }
+    @Test
+    public void test_OpenVWOLoginPage(){
+       //Open the VWO.com
+        //FirefoxDriver driver = new FirefoxDriver();
+        //ChromeDriver driver = new ChromeDriver();
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+        driver.quit();
+    }
 }
